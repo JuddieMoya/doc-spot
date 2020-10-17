c85b428acaf2dbf3e5ff65f519cbdc270ed7108c
Eleanor Hall <ruthhall97@gmail.com>
2020-10-17 16:34:30 -0700

    stuff

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import withAuthorization from 'components/hoc/withAuthorization'
-
+import  index from '../api/index'
+import db from 'db'
 const Profile = () => <h1>I am Profile Page</h1>
+
+
+
+
+
+
+export const getUserProfile = uid =>
+
+  db.collection('profiles')
+    .doc(uid)
+    .get()
+    .then(snapshot => ({uid, ...snapshot.data()}))
+    console.log('getUserProfile')
 
 export default withAuthorization(Profile)
