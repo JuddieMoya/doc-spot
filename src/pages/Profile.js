3919439773034aa799db2a0b511190e4af7377b5
Elijah Camara <elijahjohncamara@icloud.com>
2020-10-19 13:09:41 -0400

    Profile fixed

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,59 +6,56 @@ import { useDispatch } from "react-redux"
 const Profile = ({user}) => {
     const dispatch = useDispatch();
 
-    const [userInfo, setUserInfo] = useState(user);
+    let [userInfo, setUserInfo] = useState(user);
 
-    // const handleSubmit = () => {
-    //     dispatch(getUserProfile({...user, userInfo}));
-    // }
+    const getMyData = async () => {
+        let UID = firebase.auth().currentUser.uid;
+        userInfo = await getUserProfile(UID);
+        console.log("this is the user data", userInfo)
+        return userInfo
+    }
+    let currentUser = firebase.auth().currentUser;
 
-    // const getMyData = async () => {
-    //     let UID = firebase.auth().currentUser.uid;
-    //     userInfo = await getUserProfile(UID);
-    //     console.log("this is the user data", userInfo)
-    //     return userInfo
-    // }
-
-    
     firebase.auth().onAuthStateChanged(function(user) {
-        let currentUser = firebase.auth().currentUser;
+       
         let name, email, avatar, uid, emailVerified;
         if (currentUser != null) {
             name = user.displayName;
             email = user.email;
-            avatar = user.avatar;
+            avatar = user.photoURL;
             emailVerified = user.emailVerified;
             uid = user.uid;
           } else {
             return null
         }
-        console.log(user.email)
+        console.log(email)
     });
-
-    // useEffect (() => {
-    //     setUserInfo(getMyData())
-    // }, [])
-
-    const handleCancel = () => {
-        dispatch(getUserProfile(user.id));
-    };
+    
+    handleEdit = () => {
+        let toggle = docuement.getElementById("editUser");
+        if(toggle.style.display === "none") {
+            toggle.style.display = "block";
+        } else {
+            toggle.style.display = "none"
+        }
+    }
+                
+    useEffect (() => {
+        setUserInfo(getMyData())
+    }, [])
 
     return (
-        // <div className="auth-page">
-        //     <div className="container has-text-centered">
-        //         <div className="column is-4 is-offset-4">
-        //             <div className="box">
-        //                 <figure className="avatar">
-        //                     {/* <img src="" /> */}
-        //                 </figure>
-        //             </div>
-        //             {userInfo}
-        //             <button>Edit User</button>
-        //         </div>
-        //     </div>
-        // </div>
         <>
-            <h1>Hello</h1>
+            <div className="containter">
+                {/* <img src={} alt="avatar"/> */}
+                <h1>Username</h1>
+                <button onClick={handleEdit}>Edit User</button>
+                <div id="editUser">
+                    <input />
+                    <input />
+                    <input />
+                </div>
+            </div>    
         </>
     )
 }
